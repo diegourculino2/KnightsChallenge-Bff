10a4c19a08fe53ff81a18fd5be6d0faf0726c193
Diego Urçulino <diegourculino@hotmail.com>
2024-07-11 01:28:18 -0300

    fix: ajuste em cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,10 @@ import {knightsAll} from "../services/get-knights-service";
 import {knightNew} from "../services/new-knights-service";
 
 export const register = (app: express.Application) => {
+    const cors = require("cors");
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+    app.use(cors());
 
     app.get("/knights", (req: express.Request, res: express.Response) => {
         knightsAll()
@@ -17,6 +19,8 @@ export const register = (app: express.Application) => {
     });
 
     app.post("/knights", (req: express.Request, res: express.Response) => {
+        // tslint:disable-next-line:no-console
+        console.log(req.body);
         knightNew(req.body)
             .then((knights) => {
                 res.header("Access-Control-Allow-Origin", "*");
